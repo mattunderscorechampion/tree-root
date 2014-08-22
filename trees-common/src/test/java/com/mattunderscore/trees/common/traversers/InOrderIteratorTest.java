2be9c3d03e4ebc07b8873ae9ad3fa29afb761d5a
mattunderscorechampion <mattunderscorechampion@gmail.com>
2014-08-23 00:51:43 +0100

    Added inorder iterator and test

---
+++ trees-common/src/test/java/com/mattunderscore/trees/common/traversers/InOrderIteratorTest.java
@@ -0,0 +1,57 @@
+/* Copyright © 2014 Matthew Champion
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.trees.common.traversers;
+
+import com.mattunderscore.trees.IMutableNode;
+import com.mattunderscore.trees.common.LinkedTree;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * @author matt on 23/08/14.
+ */
+public final class InOrderIteratorTest {
+    @Test
+    public void test0()
+    {
+        final LinkedTree.Constructor constructor = new LinkedTree.Constructor();
+        final LinkedTree<String> tree = constructor.build("a");
+        tree.addChild("b")
+            .addChild("c");
+        tree.addChild("d");
+
+        final Iterator<IMutableNode<String>> iterator = new InOrderIterator<>(tree);
+        assertEquals("c", iterator.next().getElement());
+        assertEquals("b", iterator.next().getElement());
+        assertEquals("a", iterator.next().getElement());
+        assertEquals("d", iterator.next().getElement());
+        assertFalse(iterator.hasNext());
+    }
+}
