9adbb2932b50cc2ee85d8527ff3a2584be596483
mattunderscorechampion <mattunderscorechampion@gmail.com>
2014-11-14 20:38:26 +0000

    Added initial code for yet another path copy

---
+++ trees-internal/src/main/java/com/mattunderscore/trees/internal/pathcopy/operation/PathCopyNode.java
@@ -0,0 +1,63 @@
+/* Copyright © 2014 Matthew Champion
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.trees.internal.pathcopy.operation;
+
+import com.mattunderscore.trees.base.FixedNode;
+import com.mattunderscore.trees.collection.SimpleCollection;
+import com.mattunderscore.trees.mutable.MutableNode;
+import com.mattunderscore.trees.utilities.collections.DuplicateOnWriteSimpleCollection;
+
+/**
+ * @author matt on 14/11/14.
+ */
+public final class PathCopyNode<E> extends FixedNode<E> implements MutableNode<E> {
+    private final DuplicateOnWriteSimpleCollection<PathCopyNode<E>> children;
+
+    private PathCopyNode(E element) {
+        super(element);
+        children = DuplicateOnWriteSimpleCollection.create();
+    }
+
+    private PathCopyNode(E element, DuplicateOnWriteSimpleCollection<PathCopyNode<E>> children) {
+        super(element);
+        this.children = children;
+    }
+
+    @Override
+    public SimpleCollection<PathCopyNode<E>> getChildren() {
+        return children;
+    }
+
+    @Override
+    public boolean removeChild(MutableNode<E> child) {
+        return false;
+    }
+
+    @Override
+    public MutableNode<E> addChild(E e) {
+        return null;
+    }
+}
---
+++ trees-internal/src/test/java/com/mattunderscore/trees/internal/PathCopyTreeTest.java
@@ -0,0 +1,134 @@
+/* Copyright © 2014 Matthew Champion
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.trees.internal;
+
+import com.mattunderscore.trees.Trees;
+import com.mattunderscore.trees.collection.SimpleCollection;
+import com.mattunderscore.trees.common.TreesImpl;
+import com.mattunderscore.trees.construction.BottomUpTreeBuilder;
+import com.mattunderscore.trees.internal.pathcopy.operation.PathCopyTree;
+import com.mattunderscore.trees.mutable.MutableNode;
+import com.mattunderscore.trees.mutable.MutableTree;
+import com.mattunderscore.trees.traversal.TreeIteratorFactory;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * @author matt on 14/11/14.
+ */
+public final class PathCopyTreeTest {
+    private static TreeIteratorFactory iterators;
+    private static BottomUpTreeBuilder<String> builder;
+
+    @BeforeClass
+    public static void setUp() {
+        final Trees trees = new TreesImpl();
+        iterators = trees.treeIterators();
+        builder = trees.treeBuilders().bottomUpBuilder();
+    }
+
+    @Test
+    public void test() {
+        final MutableTree<String, MutableNode<String>> tree = builder.build(PathCopyTree.class);
+        final Iterator<String> iterator0 = iterators.inOrderElementsIterator(tree);
+        final MutableNode<String> root = tree.setRoot("root");
+        assertFalse(iterator0.hasNext());
+        assertEquals(0, root.getChildren().size());
+
+        final Iterator<String> iterator1 = iterators.inOrderElementsIterator(tree);
+        root.addChild("child");
+        assertEquals(0, root.getChildren().size());
+        assertNotEquals(root, tree.getRoot());
+        assertEquals(1, tree.getRoot().getChildren().size());
+
+        assertTrue(iterator1.hasNext());
+        assertEquals("root", iterator1.next());
+        assertFalse(iterator1.hasNext());
+
+        final Iterator<String> iterator2 = iterators.inOrderElementsIterator(tree);
+        final MutableNode<String> newRoot = tree.setRoot("newRoot");
+
+        assertTrue(iterator2.hasNext());
+        assertEquals("child", iterator2.next());
+        assertEquals("root", iterator2.next());
+        assertFalse(iterator2.hasNext());
+
+        final MutableNode<String> newChild = newRoot.addChild("newChild");
+        final Iterator<String> iterator3 = iterators.inOrderElementsIterator(tree);
+        assertTrue(tree.getRoot().removeChild(newChild));
+        final Iterator<String> iterator4 = iterators.inOrderElementsIterator(tree);
+
+        assertTrue(iterator3.hasNext());
+        assertEquals("newChild", iterator3.next());
+        assertEquals("newRoot", iterator3.next());
+        assertFalse(iterator3.hasNext());
+        assertTrue(iterator4.hasNext());
+        assertEquals("newRoot", iterator4.next());
+        assertFalse(iterator4.hasNext());
+
+        assertFalse(newRoot.removeChild(root));
+    }
+
+    @Test
+    public void mutateTree() {
+        final MutableTree<String, MutableNode<String>> tree = builder.build(PathCopyTree.class);
+        tree.setRoot("a");
+        final MutableNode<String> root = tree.getRoot();
+        assertTrue(root.isLeaf());
+        final MutableNode<String> depth1 = root.addChild("b");
+        assertFalse(root.isLeaf());
+        depth1.addChild("c");
+
+        final SimpleCollection<? extends MutableNode<String>> children = root.getChildren();
+        assertEquals(1, children.size());
+        final Iterator<? extends MutableNode<String>> iterator0 = children.iterator();
+        assertTrue(iterator0.hasNext());
+        final MutableNode<String> child0 = iterator0.next();
+        assertEquals("b", child0.getElement());
+
+        final Iterator<? extends MutableNode<String>> iterator1 = child0.getChildren().iterator();
+        assertTrue(iterator1.hasNext());
+        final MutableNode<String> child1 = iterator1.next();
+        assertEquals("c", child1.getElement());
+        assertFalse(iterator1.hasNext());
+
+        depth1.addChild("d");
+        final Iterator<? extends MutableNode<String>> iterator2 = child0.getChildren().iterator();
+        assertTrue(iterator2.hasNext());
+        final MutableNode<String> child2 = iterator2.next();
+        assertEquals("c", child2.getElement());
+        assertTrue(iterator2.hasNext());
+        final MutableNode<String> child3 = iterator2.next();
+        assertEquals("d", child3.getElement());
+        assertFalse(iterator2.hasNext());
+    }
+}
