d8bfe3eb0e34a2ca43b9ed878ea0ffd0b328bfa1
Matt Champion <mattunderscorechampion@gmail.com>
2015-02-10 22:38:21 +0000

    Added test

---
+++ trees-api/src/test/java/com/mattunderscore/trees/traversal/DefaultTreeWalkerTest.java
@@ -0,0 +1,45 @@
+package com.mattunderscore.trees.traversal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DefaultTreeWalkerTest {
+    private static final DefaultTreeWalker WALKER = new DefaultTreeWalker() {
+    };
+
+    @Test
+    public void testOnStarted() throws Exception {
+        WALKER.onStarted();
+    }
+
+    @Test
+    public void testOnNode() throws Exception {
+        WALKER.onNode(null);
+    }
+
+    @Test
+    public void testOnNodeChildrenStarted() throws Exception {
+        WALKER.onNodeChildrenStarted(null);
+    }
+
+    @Test
+    public void testOnNodeChildrenRemaining() throws Exception {
+        WALKER.onNodeChildrenStarted(null);
+    }
+
+    @Test
+    public void testOnNodeChildrenCompleted() throws Exception {
+        WALKER.onNodeChildrenCompleted(null);
+    }
+
+    @Test
+    public void testOnNodeNoChildren() throws Exception {
+        WALKER.onNodeNoChildren(null);
+    }
+
+    @Test
+    public void testOnCompleted() throws Exception {
+        WALKER.onCompleted();
+    }
+}
