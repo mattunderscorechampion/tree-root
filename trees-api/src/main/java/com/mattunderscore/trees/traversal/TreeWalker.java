7ddb33cbd8162edb17e53c4d6da860028b62758c
Matt Champion <mattunderscorechampion@gmail.com>
2015-02-10 22:21:42 +0000

    Added comments explaining the order of calls

--- trees-api/src/main/java/com/mattunderscore/trees/traversal/TreeWalker.java
+++ trees-api/src/main/java/com/mattunderscore/trees/traversal/TreeWalker.java
@@ -1,7 +1,5 @@
 package com.mattunderscore.trees.traversal;
 
-import com.mattunderscore.trees.tree.Node;
-
 /**
  * Interface for walking over a tree. Unlike a {@link Walker} it provides information on a nodes children. Provides
  * internal iterator.
@@ -9,17 +7,43 @@ import com.mattunderscore.trees.tree.Node;
  * @author Matt Champion on 31/01/15
  */
 public interface TreeWalker<E> {
+    /**
+     * Called on the start of the iteration.
+     */
     void onStarted();
 
+    /**
+     * Called on visiting a node.
+     * @param node The node
+     */
     void onNode(E node);
 
+    /**
+     * Called before the first child of a node is visited.
+     * @param node The node
+     */
     void onNodeChildrenStarted(E node);
 
+    /**
+     * Called after a child node (and its children) have been visited and before the next child node is visited.
+     * @param node The parent node
+     */
     void onNodeChildrenRemaining(E node);
 
+    /**
+     * Called after all child nodes (and their children) have been visited and before the next sibling node is visited.
+     * @param node The node
+     */
     void onNodeChildrenCompleted(E node);
 
+    /**
+     * Called when the last visited node has no children.
+     * @param node The node with no children
+     */
     void onNodeNoChildren(E node);
 
+    /**
+     * Called at the end of the iteration.
+     */
     void onCompleted();
 }
