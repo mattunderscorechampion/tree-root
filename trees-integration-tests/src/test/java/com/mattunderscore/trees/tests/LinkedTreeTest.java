3d6de509b19d328497f5282c70852a0f87d8c2d3
Matt Champion <mattunderscorechampion@gmail.com>
2015-04-17 21:16:50 +0100

    Moved linked tree to separate module

--- trees-integration-tests/src/test/java/com/mattunderscore/trees/tests/LinkedTreeTest.java
+++ trees-integration-tests/src/test/java/com/mattunderscore/trees/tests/LinkedTreeTest.java
@@ -23,10 +23,12 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-package com.mattunderscore.trees.common;
+package com.mattunderscore.trees.tests;
 
 import com.mattunderscore.trees.Trees;
 import com.mattunderscore.trees.collection.SimpleCollection;
+import com.mattunderscore.trees.common.LinkedTree;
+import com.mattunderscore.trees.common.TreesImpl;
 import com.mattunderscore.trees.construction.BottomUpTreeBuilder;
 import com.mattunderscore.trees.mutable.MutableNode;
 import com.mattunderscore.trees.tree.Node;
