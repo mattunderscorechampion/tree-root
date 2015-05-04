1180dc009458fc8a8bb772ad5a2571f8e89678a5
Matt Champion <mattunderscorechampion@gmail.com>
2015-05-04 17:53:53 +0100

    Added test for PathCopy tree node to tree converter

---
+++ trees-pathcopy/src/test/java/com/mattunderscore/trees/pathcopy/holder/NodeConverterTest.java
@@ -0,0 +1,74 @@
+/* Copyright © 2015 Matthew Champion
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of mattunderscore.com nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MATTHEW CHAMPION BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.mattunderscore.trees.pathcopy.holder;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Iterator;
+
+import org.junit.Test;
+
+import com.mattunderscore.trees.Trees;
+import com.mattunderscore.trees.construction.BottomUpTreeBuilder;
+import com.mattunderscore.trees.construction.TopDownTreeRootBuilder;
+import com.mattunderscore.trees.impl.SPISupport;
+import com.mattunderscore.trees.impl.TreesImpl;
+import com.mattunderscore.trees.mutable.MutableNode;
+import com.mattunderscore.trees.mutable.MutableTree;
+
+/**
+ * Test for {@link NodeConverter}.
+ * @author Matt Champion on 04/05/15
+ */
+public final class NodeConverterTest {
+      private static final Trees trees = new TreesImpl();
+
+      @Test
+      public void convertToSelf() {
+            final BottomUpTreeBuilder<Object> builder = trees.treeBuilders().bottomUpBuilder();
+            final PathCopyTree<String> tree = builder.create("a", builder.create("b"), builder.create("c")).build(PathCopyTree.class);
+
+            final NodeConverter<String> converter = new NodeConverter<>();
+
+            final PathCopyTree<String> convertedTree =
+                converter.treeFromRootNode(tree.getRoot());
+
+            final MutableNode<String> convertedRoot = convertedTree.getRoot();
+            assertEquals(1, convertedRoot.getNumberOfChildren());
+            final Iterator<? extends MutableNode<String>> iterator0 = convertedRoot.childIterator();
+            assertTrue(iterator0.hasNext());
+            final MutableNode<String> child0 = iterator0.next();
+            assertEquals("b", child0.getElement());
+
+            final Iterator<? extends MutableNode<String>> iterator1 = child0.childIterator();
+            assertTrue(iterator1.hasNext());
+            final MutableNode<String> child1 = iterator1.next();
+            assertEquals("c", child1.getElement());
+            assertFalse(iterator1.hasNext());
+      }
+}
