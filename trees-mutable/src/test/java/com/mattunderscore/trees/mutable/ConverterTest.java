23f4eb57b6d2f2f5849fc7896dd6d47847091fdd
Matt Champion <mattunderscorechampion@gmail.com>
2015-08-10 22:27:17 +0100

    Added explicit test for mutable tree converter.

---
+++ trees-mutable/src/test/java/com/mattunderscore/trees/mutable/ConverterTest.java
@@ -0,0 +1,60 @@
+package com.mattunderscore.trees.mutable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mattunderscore.trees.Trees;
+import com.mattunderscore.trees.construction.BottomUpTreeBuilder;
+import com.mattunderscore.trees.impl.TreesImpl;
+import com.mattunderscore.trees.linked.tree.LinkedTree;
+
+/**
+ * Unit tests for {@link Converter}.
+ *
+ * @author Matt Champion on 10/08/2015
+ */
+public final class ConverterTest {
+    private LinkedTree<String> tree;
+
+    @Before
+    public void setUp() {
+        final Trees trees = new TreesImpl();
+
+        final BottomUpTreeBuilder<String, MutableSettableStructuredNode<String>> builder =
+            trees.treeBuilders().bottomUpBuilder();
+
+        tree = builder.create("a",
+            builder.create("b"),
+            builder.create("c",
+                builder.create("d"))).build(LinkedTree.typeKey());
+    }
+
+    @Test
+    public void convert() {
+        final Converter<String> converter = new Converter<>();
+
+        final MutableTreeImpl<String> mutableTree = converter.build(tree);
+
+        assertEquals("a", mutableTree.getRoot().getElement());
+        final Iterator<MutableTreeImpl<String>> rootChildIterator = mutableTree.getRoot().childIterator();
+        assertEquals("b", rootChildIterator.next().getElement());
+        final MutableTreeImpl<String> child = rootChildIterator.next();
+        assertFalse(rootChildIterator.hasNext());
+        assertEquals("c", child.getElement());
+        final Iterator<MutableTreeImpl<String>> childChildIterator = child.childIterator();
+        assertEquals("d", childChildIterator.next().getElement());
+        assertFalse(childChildIterator.hasNext());
+    }
+
+    @Test
+    public void forClass() {
+        final Converter<String> converter = new Converter<>();
+
+        assertEquals(MutableTreeImpl.class, converter.forClass());
+    }
+}
